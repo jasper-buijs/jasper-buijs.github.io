51871e346209805e4b439f86d346a11f165d2140
Jasper Buijs <jabuijs@icloud.com>
2023-12-23 21:16:54 +0100

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="text-center">
+      <h1 className="mt-[35vh] text-7xl">{":("}</h1>
+      <p className="mt-4 mb-8">404 - Sorry, we couldn't find the page you were looking for.</p>
+      <a className="rounded-md p-2 bg-[#cfb53b] text-[#ffffff] w-auto" href="/">Take me home!</a>
+    </div>
+  );
+}
+
+export default NotFound;
