bcdc97af862021409eb709c4204c4b7924aaed8e
Jasper Buijs <jabuijs@icloud.com>
2024-02-05 22:58:49 +0100

    minercaft page updated for modded server

--- app/minecraft/page.tsx
+++ app/minecraft/page.tsx
@@ -12,25 +12,36 @@ const MinecraftPage = () => {
   return (
     <>
       <NavBar activePage="minecraft" />
-      <Alert type="warning" text="The Minecraft server is currently down due to low demand. It may return after the exam period, maybe with a new world. If you still want to play on the old server, send a message in our Discord and I'll try to boot up the server as soon as possible." />
-      <img src="/logo-mc.png" className="block ml-auto mr-auto" />
-      <h1 className="text-center text-3xl mt-4">Heilige Maagden Minecraft Server</h1>
+      {/*<Alert type="warning" text="The Minecraft server is currently down due to low demand. It may return after the exam period, maybe with a new world. If you still want to play on the old server, send a message in our Discord and I'll try to boot up the server as soon as possible." />*/}
+      <div className="block w-max ml-auto mr-auto mt-8">
+        <img src="/logo-mc.png" className="inline-block mr-8 h-64" />
+        <a href="https://www.curseforge.com/minecraft/modpacks/minecolonies-dimensional-adventure" className="inline-block">
+          <img src="https://cdn.modrinth.com/data/HqE7wBYb/e4fc44e09ef18ec432a21b1d1675c0fdf1788bca.jpeg" className="inline-block h-64" />
+        </a>
+      </div>
+      <h1 className="text-center text-3xl mt-4">MineColonies: Dimensional Adventure</h1>
       <Link href="" onClick={() => navigator.clipboard.writeText("minecraft.heiligemaagden.com")} className=" block text-center text-lg text-[#cfb53b] underline">
         minecraft.heiligemaagden.com
       </Link>
       <Paragraph emoji="ℹ️" title="General Info">
-        Our server is back! New start, new world, same concept. We play in hard-difficulty survival, with PvP enabled but (usually) not the essence of the server. Everyone is free to join on <b>minecraft.heiligemaagden.com</b>. The server is vanilla-ish - though we run some mods to optimise game handling barely anything in-game gets changed. We also use four datapacks to alter the game behavior slightly:
-        <ol>
-          <li className="pl-2">1. One adds gravestones to the game: when a player die, a gravestone will spawn with their items where they died, which can be collected. This way, when you die, you don't have to rush to get your stuff within the despawn time.</li>
-          <li className="pl-2">2. A second one makes it so item frames that are currently holding items will turn invisible. This allows you to use item frames for decorational purposes without heaving the ugly leather background appear.</li>
-          <li className="pl-2">3. We have one that makes nametags craftable. To make one, you'll need two leather or rabbit hide and one string. 4. Lastly, we have one that pauses the game when no one is online, that way the total age of the server will beter represent overall playtime by our players.</li>
-        </ol>
+        Our server is back, and this time it's modded. We've relaunched our server with the MineColonies: Dimensional Adventure modpack and Forge mod loader.
+        <br />
+        <br />
+        "Embark on a world of creation and discovery with MineColonies: Dimensional Adventure. This modpack elevates your Minecraft experience from mere building to crafting stories, growing communities, and carving out your own unique journey. As the architect of your domain, breathe life into your colony one block at a time. Let your town bristle with activity, every corner buzzing with the life you brought into the world. But don't get too comfortable! Beyond the cozy lanes of your town lie uncharted dimensions, eerie dungeons, and distant planets, all waiting for a daring explorer to uncover their secrets."
       </Paragraph>
-      <Paragraph emoji="🛠️" title="Daily Blackout Period">
-        Every day, our server will go offline between 6 am and 7:30 am. If you're still actively playing, you will be warned and should probably disconnect. If you're AFK you'll be kicked. The server will begin to start up at 7:30 am, though it can take some time for it to get back online. You should really only be connecting starting around 7:40 am if possible.
+      <Paragraph emoji="🛟" title="Mods Recuired To Play">
+        In order to play, you'll also need to install the modpack clientsided. You can do this using the Curseforge App or the Prism Launcher. There, download and install{" "}
+        <a href="https://www.curseforge.com/minecraft/modpacks/minecolonies-dimensional-adventure" className="text-blue-800 underline">
+          the modpack
+        </a>{" "}
+        in order to play.
       </Paragraph>
-      <Paragraph emoji="🛟" title="Useful Resources">
-        In Discord, we've created a "server-coords" channel where you can share and/or save important coordinates from our server. Also, if you want to know weather the server is up, you can check the status page on <a href="https://admin.minecraft.heiligemaagden.com/status">https://admin.minecraft.heiligemaagden.com/status</a>. It may warn you of being unsafe; it's not, though I won't oblige you to continue. If that page is down as well, not only our server but also it's host have been turned off or disconnected, meanning the server will be offline for sure.
+      <Paragraph emoji="🌐" title="Server Up-Time">
+        The server won't always be online. If you want to play when the server is offline, send us a message in Discord. You can check whether the server is online, and how many players are active, on{" "}
+        <a href="https://minecraft.heiligemaagden.com:8443/status" className="text-blue-800 underline">
+          this site
+        </a>
+        . Your browser may mark this as unsafe as this page is locally hosted, but you can safely continue. When this page is offline, the server will be too.
       </Paragraph>
       <FooterBar />
     </>
