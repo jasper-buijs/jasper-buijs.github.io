b663f1f095733603864f92a21d463a091f2f4ae5
Jasper Buijs <62234929+jasper-buijs@users.noreply.github.com>
2024-03-20 20:34:18 +0100

    Australia schedule adjustments

--- app/formulaone/races/australia/page.tsx
+++ app/formulaone/races/australia/page.tsx
@@ -111,7 +111,7 @@ const Page = () => {
               <td className="pr-2">{f1}Qualifying</td>
               <td className="pr-2">{[new Date("March 23, 2024 16:00:00 UTC+11:00").toLocaleString(undefined, { weekday: "long", hour: "2-digit", minute: "2-digit" }), "-", new Date("March 23, 2024 17:00:00 UTC+11:00").toLocaleString(undefined, { hour: "2-digit", minute: "2-digit" })].join(" ")}</td>
               <td className="pr-2">18' + 15' + 12'</td>
-              <td className="pr-2">{auvio}</td>
+              <td className="pr-2">{hm}{auvio}</td>
             </tr>
             <tr>
               <td className="h-[2rem]"></td>
@@ -136,7 +136,7 @@ const Page = () => {
               <td className="pr-2">{f1}Race</td>
               <td className="pr-2">{[new Date("March 24, 2024 15:00:00 UTC+11:00").toLocaleString(undefined, { weekday: "long", hour: "2-digit", minute: "2-digit" }), "-", new Date("March 24, 2024 17:00:00 UTC+11:00").toLocaleString(undefined, { hour: "2-digit", minute: "2-digit" })].join(" ")}</td>
               <td className="pr-2">58L or 2h+1L</td>
-              <td className="pr-2">{auvio}</td>
+              <td className="pr-2">{hm}{auvio}</td>
             </tr>
           </tbody>
         </table>
