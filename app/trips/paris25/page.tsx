06767cd3d871f44b0c428930a3e5dcab64c62fa8
Jasper Buijs <jabuijs@icloud.com>
2025-03-09 13:10:37 +0100

    Paris page redirect added

---
+++ app/trips/paris25/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import { redirect } from "next/navigation";
+
+export default function Page() {
+    redirect("https://my.tripsy.app/i/649574?code=7c5d2f886ac771adfb09cacbe3a1205b");
+    return (<></>);
+}
