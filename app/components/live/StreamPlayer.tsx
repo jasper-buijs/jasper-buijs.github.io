5a789d9d39296efb70427c1163899a8ed8bd4c2d
Jasper Buijs <jabuijs@icloud.com>
2025-07-25 13:55:00 +0200

    Fix video/audio controls

--- app/components/live/StreamPlayer.tsx
+++ app/components/live/StreamPlayer.tsx
@@ -2,14 +2,14 @@
 
 import {
   AudioTrack,
-  useParticipants,
+  useParticipants, useStartAudio, useStartVideo,
   useTracks,
   VideoTrack,
 } from "@livekit/components-react";
 import { Track } from "livekit-client";
-import { CircleUserRound, Volume2, VolumeOff } from "lucide-react";
+import { CircleUserRound, Expand, Pause, Play, Volume2, VolumeOff } from "lucide-react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // Poster images (should be 16:9)
 /*const liveImages = [
@@ -42,12 +42,20 @@ const liveBgs = [
 
 const StreamPlayer = () => {
   // const { name: roomName, state: roomState } = useRoomContext();
+  // const roomInstance = useRoomContext();
   const participants = useParticipants();
   const remoteVideoTracks = useTracks([Track.Source.ScreenShare]);
   const remoteAudioTracks = useTracks([Track.Source.ScreenShareAudio]);
 
-  const [ muted, setMuted ] = useState<boolean>(true);
+  const [ muted, setMuted ] = useState<boolean>(false);
   const [ volume, setVolume ] = useState<number>(1);
+
+  const { mergedProps: audioProps, canPlayAudio } = useStartAudio({ props: {id: "a"} });
+  const { mergedProps, canPlayVideo } = useStartVideo({ props: audioProps });
+  const { style, ...restProps } = mergedProps;
+  style.display = canPlayAudio && canPlayVideo ? 'none' : 'block';
+
+  const videoElement = useRef<HTMLVideoElement>(null);
 
   const [ bgImage, setBgImage ] = useState<`bg-[url(/bgs/live${number}.png)]`>(); //liveBgs[Math.floor(Math.random() * liveBgs.length)]
   // const [ poster, setPoster ] = useState<`/bgs/live${number}.png`>();
@@ -70,16 +78,66 @@ const StreamPlayer = () => {
       <div className={"block fixed z-10 top-20 bottom-20 left-12 right-12 overflow-hidden"}>
         <div className={"h-full w-full flex items-center justify-left"}>
           { remoteVideoTracks.map((track) => (
-            <div
-              key={track.participant.identity + remoteVideoTracks.indexOf(track) + "vid"}
-              className={"flex-shrink-0 max-h-full max-w-full aspect-video"}
-            >
-              <VideoTrack
-                trackRef={track}
-                className={`!w-full !max-w-full object-cover bg-black`}
-                controls={true}
-              />
-            </div>
+            <>
+              <div
+                key={track.participant.identity + remoteVideoTracks.indexOf(track) + "vid"}
+                className={"flex-shrink-0 h-full max-w-full aspect-[1920/1120]"}
+              >
+                <VideoTrack
+                  trackRef={track}
+                  className={`!h-[calc(100%-40px)] !w-fit !max-w-full object-cover bg-black`}
+                  controls={false}
+                  autoPlay={false}
+                  ref={videoElement}
+                />
+                <div className={"h-8 mt-2 w-full flex"}>
+                  {/*<StartMediaButton
+                    className={`h-8 aspect-square !bg-gray-500 !rounded-md !p-1 !mr-2 inline-block align-middle justify-start`}
+                  >
+                    <Play />
+                  </StartMediaButton>*/}
+                  <button
+                    className={`h-8 aspect-square !bg-gray-500 !rounded-md !p-1 !mr-2 inline-block align-middle justify-start`}
+                    onClick= { mergedProps.onClick }
+                    style={style}
+                  >
+                    <Play />
+                  </button>
+                  { canPlayVideo && canPlayAudio &&
+                    <button
+                      onClick={() => {
+                        videoElement.current?.paused ? setMuted(false) : setMuted(true);
+                        videoElement.current?.paused ? videoElement.current.play() : videoElement.current?.pause();
+                      }}
+                      className={"h-8 aspect-square bg-gray-500 rounded-md p-1 mr-2 inline-block align-middle"}
+                    >
+                      {videoElement.current?.paused ? <Play /> : <Pause />}
+                    </button>
+                  }
+                  <button
+                    onClick={() => videoElement.current?.requestFullscreen({ navigationUI: "hide" })}
+                    className={"h-8 aspect-square bg-gray-500 rounded-md p-1 mr-2 inline-block align-middle"}
+                  >
+                    <Expand />
+                  </button>
+                  <button
+                    onClick={() => setMuted(m => !m)}
+                    className={"h-8 aspect-square bg-gray-500 rounded-md p-1 mr-2 inline-block align-middle"}
+                  >
+                    {muted ? <VolumeOff /> : <Volume2 />}
+                  </button>
+                  <input
+                    type="range"
+                    min={0}
+                    max={1}
+                    step={0.1}
+                    value={volume}
+                    onChange={e => setVolume(Number(e.target.value))}
+                    className={"inline-block h-8 bg-gray-500 w-[calc(100%-40px)] max-w-52 align-middle"}
+                  />
+                </div>
+              </div>
+            </>
           ))}
           { (!init || (remoteVideoTracks.length == 0 && participants.some(p => p.identity.includes("ingress")))) && (
             <>
@@ -125,23 +183,8 @@ const StreamPlayer = () => {
                 )
               )}
             </div>
-            <div className={"h-[20%] content-end"}>
-              <button
-                onClick={() => setMuted(m => !m)}
-                className={"h-8 aspect-square bg-gray-500 rounded-md p-1 mr-2 inline-block align-middle"}
-              >
-                {muted ? <VolumeOff /> : <Volume2 />}
-              </button>
-              <input
-                type="range"
-                min={0}
-                max={1}
-                step={0.01}
-                value={volume}
-                onChange={e => setVolume(Number(e.target.value))}
-                className={"inline-block h-8 bg-gray-500 w-[calc(100%-40px)] align-middle"}
-              />
-            </div>
+            {/*<div className={"h-[20%] content-end"}>
+            </div>*/}
           </div>
         </div>
       </div>
